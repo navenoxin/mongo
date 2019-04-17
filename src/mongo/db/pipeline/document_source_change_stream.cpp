4fcb548d16a3d60ed305cf5e1833ad2b1129477c
Evan Nixon <evan.nixon@10gen.com>
2019-04-16 19:01:05 -0700

    format

--- src/mongo/db/pipeline/document_source_change_stream.cpp
+++ src/mongo/db/pipeline/document_source_change_stream.cpp
@@ -275,9 +275,10 @@ BSONObj DocumentSourceChangeStream::buildMatchFilter(
     // from a chunk migration. Do not filter those operations otherwise. On mongos, this translates
     // to all operations from (2.1) or (2.2) that are not part of a chunk migration. On mongod, this
     // is all operations from (2.1) or (2.2).
-    BSONObj opMatch = (expCtx->fromMongos
-        ? BSON("$and" << BSON_ARRAY(normalOrChunkMigratedMatch << notFromMigrateFilter))
-        : normalOrChunkMigratedMatch);
+    BSONObj opMatch =
+        (expCtx->fromMongos
+             ? BSON("$and" << BSON_ARRAY(normalOrChunkMigratedMatch << notFromMigrateFilter))
+             : normalOrChunkMigratedMatch);
 
     // 3) Look for 'applyOps' which were created as part of a transaction.
     BSONObj applyOps = getTxnApplyOpsFilter(opNsMatch["ns"], nss);
