09bf8888329a2485dfe21e27085f4444f05bf16c
Evan Nixon <evan.nixon@10gen.com>
2019-05-21 15:32:11 -0400

    extra newline

--- jstests/sharding/change_stream_chunk_migration.js
+++ jstests/sharding/change_stream_chunk_migration.js
@@ -1,7 +1,6 @@
 // Tests that change stream returns the stream of results continuously and in the right order when
 // it's migrating a chunk to a new shard. Also tests the undocumented 'showChunkMigrations' option.
 // @tags: [uses_change_streams]
-
 (function() {
     'use strict';
 
